4b9b5f47310bdb57bcf5d821d07fb7a6cb72ee58
Glauco Santos <gpcsantos@gmail.com>
2024-11-04 11:53:58 -0300

    desafio node + nginx

---
+++ node/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql2');
+
+const insertName = async (nome) => {
+    const connection = mysql.createConnection(config)
+    const sql = `INSERT INTO people(name) values('${nome}')`
+    connection.query(sql)
+    connection.end()
+}
+
+const searchNames = async (res) => {
+    let page = '<h1>Full Cycle Rocks!</h1>\n<ul>'
+    const connection = mysql.createConnection(config)
+    const sql = `SELECT * FROM people`
+    connection.query(
+        sql,
+        (error, results, fields) => {
+            if (error) {
+                console.log(error);
+                mysql.end();
+                res.send(`<h1>${error}</h1>`);
+            }
+            if (results.length > 0) {
+                console.log(results);
+                results.forEach(element => {
+                    page += `\n<li>nameid [${element['id']}]: ${element['name']}</li>`
+                });
+                page += `\n</ul>`
+                res.send(page)
+            } else {
+                res.send(page)
+            }
+        }
+    )
+    connection.end()
+}
+
+app.get('/', async (req, res) => {
+    await insertName('Glauco P C Santos');
+    await searchNames(res);
+})
+
+app.listen(port, ()=>{
+    console.log('Rodando na porta, ' + port);
+})
